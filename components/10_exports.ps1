377fd711cdf3362100d50d77dbfaaf6bef3bfbc1
Andrejus Kostarevas <andrejusk@github.com>
2024-11-30 16:03:07 +0000

    feat: remove powershell components and dotfiles

--- components/10_exports.ps1
+++
@@ -1,5 +0,0 @@
-Set-Environment "EDITOR" "code"
-Set-Environment "GIT_EDITOR" $Env:EDITOR
-
-Set-Environment "WORKSPACE" "$HOME/workspace"
-New-Item $env:WORKSPACE -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
